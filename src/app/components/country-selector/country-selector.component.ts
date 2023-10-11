df64a3fe8d05db6017c0532200d1f374ae9efb87
witoldolszyk <witoldolszyk@gmail.com>
2023-10-12 01:00:35 +0200

    exports removed

--- src/app/components/country-selector/country-selector.component.ts
+++ src/app/components/country-selector/country-selector.component.ts
@@ -1,8 +1,4 @@
 import { Component} from '@angular/core';
-import { Standing, TeamStandings } from 'src/app/models/standing.interface';
-import { FootballLeagueResultsService } from 'src/app/services/football-league-results.service';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-country-selector',
